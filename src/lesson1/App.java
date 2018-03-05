713482071003c044c1e6464c7e7ec9b175fb9ae9
ivanchik083 <antropov083@gmail.com>
2018-03-05 22:05:23 +0300

    New

---
+++ src/lesson1/App.java
@@ -0,0 +1,32 @@
+package lesson1;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class App {
+
+        private static final String FILE = "Monitor.txt";
+
+    public static void main(String[] args)  {
+
+        try {
+            Monitor monitor = new Monitor("Dell", "RLO1234", "9865");
+            final FileOutputStream fos = new FileOutputStream(FILE);
+            final ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(monitor);
+            oos.close();
+
+            System.out.println(monitor);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
--- src/lesson1/Monitor.java
+++ src/lesson1/Monitor.java
@@ -1,10 +1,51 @@
 package lesson1;
 
-public class Monitor {
+import java.io.IOException;
+import java.io.Serializable;
+
+public class Monitor implements Serializable {
+
+    private String brand;
+    private String model;
+    private String price;
+
+    public Monitor() {}
 
 
+    public Monitor(String brand, String model, String price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
 
+    public String getBrand() {
+        return brand;
+    }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 
+    public String getModel() {
+        return model;
+    }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return  "Monitor: " + "\n" +
+                " Brand = " + brand + "\n" +
+                " Model = " + model + "\n" +
+                " Price = " + price;
+    }
 }
